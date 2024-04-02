b8885075a9c84578110d9c195e2b4a7a1bfbbca6
SnikiEP <71599139+SnikiEP@users.noreply.github.com>
2024-04-02 12:58:28 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
   s3: {
-    accessKeyId: '', // TODO
-    secretAccessKey: '', // TODO
-    endpoint: '', // TODO
-    bucket: '', // TODO
+    accessKeyId: 'F4BH5FH7TDUVNJT4BSTY', // TODO
+    secretAccessKey: 'Pz693X90OXQzsgFHK5TGFZMQJVArTrcuzmylCU4U', // TODO
+    endpoint: 's3.eu-central-1.wasabisys.com', // TODO
+    bucket: 'esgi-cloud-exam-bucket-37604', // TODO
   },
   suffix: {
     small: '_small.jpg',
